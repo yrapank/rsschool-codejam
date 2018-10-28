06c754c9b735216c90b76ccb04a54100f222da92
Yura Pon <yra.pank@gmail.com>
2018-10-28 15:59:23 +0300

    1st task done !

---
+++ sumOfOther.js
@@ -0,0 +1,13 @@
+ const sumOfOther = function(arr){
+  let ans =[];
+  for(let i = 0; i<arr.length; i++){
+    let sum = 0;
+    for(let j =0; j<arr.length; j++){
+      sum+=arr[j]
+    }
+    sum-=arr[i];
+    ans.push(sum);
+  }
+  return ans
+}
+console.log(sumOfOther([2, 3, 4, 1]));
